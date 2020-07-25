727b2c48c2be17d339f0c1a08f73ce7d928e37fc
Cristiano da Silva Ferreira <cristianoT@users.noreply.github.com>
2020-07-24 23:39:29 -0300

    Erro no salvamento do endereço do cliente! Problema resolvido!!

--- lib/screens/cart/cart_sreen.dart
+++ lib/screens/cart/cart_sreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_drawer/empty_cart.dart';
+import 'package:lojavirtual/common/custom_drawer/login_cart.dart';
 import 'package:lojavirtual/common/custom_drawer/price_card.dart';
 import 'package:lojavirtual/models/cart_manager.dart';
 import 'package:lojavirtual/screens/cart/components/cart_tile.dart';
@@ -14,6 +16,19 @@ class CartScreen extends StatelessWidget {
       ),
       body: Consumer<CartManager>(
         builder: (_, cartManager, __) {
+        // Verificando se o usuário esta logado
+        if (cartManager.user == null) {
+          return LoginCart();
+        }
+
+        // Verificando se tem itens para exibir no carrinho
+        if (cartManager.items.isEmpty) {
+          return EmptyCard(
+            iconData: Icons.remove_shopping_cart,
+            title: 'Nenhum produto no carrinho',
+          );
+        }
+
         return ListView(
           children: <Widget>[
             Column(
@@ -24,11 +39,11 @@ class CartScreen extends StatelessWidget {
             PriceCard(
               buttonText: 'Continuar para Entrega',
               onPressed: cartManager.isCartValid ? () {
-                Navigator.of(context).pushNamed('/address');
-              } : null,
-            ),
-          ],
-        );
+                  Navigator.of(context).pushNamed('/address');
+                } : null,
+              ),
+            ],
+          );
         }
       ),
     );
